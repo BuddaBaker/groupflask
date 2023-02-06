f44f8fa5600b459d805b738d72369c476105efe9
BuddaBaker <prasithchilla@gmail.com>
2023-02-05 20:57:17 -0800

    python jeopardy

--- model/jeopardy.py
+++ model/jeopardy.py
@@ -1,18 +1,42 @@
-from flask import Flask, jsonify
 
-app = Flask(name)
 
 questions = [
     {
-        'category': 'History',
-        'question': 'Who was the first president of the United States?',
-        'answer': 'George Washington'
+        'category': 'UFC',
+        'question': 'Who won the Welterweight title from Tyron Woodley',
+        'answer': 'Kamaru Usman',
+        'points' : '100'
+    },
+    {
+        'category': 'UFC',
+        'question': 'Who has most title defenses of all time?',
+        'answer': 'Demetrius Johnson',
+        'points' : '200'
+    },
+    {    
+        'category': 'UFC',
+        'question': 'Who was the first UFC Champ from England',
+        'answer': 'Michael Bisping',
+        'points' : '300'
+    },
+    {
+        'category': 'UFC',
+        'question': 'Who beat Connor Mcgregor at UFC 229?',
+        'answer': 'Khabib',
+        'points' : '400'
+    },
+    
+    {
+        'category': 'UFC',
+        'question': 'Who has most wins in the UFC all time before a champsionship fight?',
+        'answer': 'Jorge Masvidal',
+        'points' : '500'
     },
     {
         'category': 'Science',
         'question': 'What is the chemical symbol for hydrogen?',
         'answer': 'H'
-    }
+    },
 ]
 
 @app.route('/questions', methods=['GET'])
