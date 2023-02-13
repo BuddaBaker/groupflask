79ac7cdee98db639e83c14f9af91ebe093c4f538
BuddaBaker <prasithchilla@gmail.com>
2023-02-13 15:28:40 -0800

    commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import threading
-import model.jeopardy
+
 
 
 # import "packages" from flask
@@ -45,6 +45,5 @@ def activate_job():
 # this runs the application on the development server
 if __name__ == "__main__":
     # change name for testing
-    from flask_cors import CORS
-    cors = CORS(app)
+    
     app.run(debug=True, host="0.0.0.0", port="8086")
